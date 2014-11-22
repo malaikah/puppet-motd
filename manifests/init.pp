a628e27bb1921d690e5059144fd75ebc1d3e946a
Ben Priestman <ben.priestman@immediate.co.uk>
2014-11-22 23:46:43 +0000

    Updating out-of-date dependencies. \nAdding boilerplate comments on manifests. \nStreamlining redundant logic on register.pp. \nIncluding init in type. \nAdding ability to specify header template.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,24 @@
+# == Class: motd
+#
+# configures concat target file with, optionally, a header.
+#
+# == parameters
+#
+# [*motd*]
+#  The path to our motd file
+#
+# [*headers*]
+#  If true, create a header file at the top of the concatted motd file.
+#  Defaults to false
+#
+# [*template*]
+#  Template to use to create headers.
+#  Defaults to a rather fetching graphic.
+#
 class motd (
-  $motd    = '/etc/motd',
-  $headers = false,
+  $motd     = '/etc/motd',
+  $headers  = false,
+  $template = 'motd/header.erb',
 ) {
 
   concat { $motd:
@@ -14,7 +32,7 @@ class motd (
   {
     concat::fragment{'motd_header':
       target  => $motd,
-      content => template('motd/header.erb'),
+      content => template($template),
       order   => 01,
     }
   }
--- manifests/register.pp
+++ manifests/register.pp
@@ -1,15 +1,27 @@
-define motd::register($content='', $order=10) {
-  if($content == '')
-  {
-    $body = "    -- ${name}\n"
-  }
-  else
-  {
-    $body = $content
-  }
+# == Define: motd::register
+#
+# Can be used to add snippets to our motd file.
+#
+# == Parameters
+#
+# [*content*]
+#  The content of our fragment.
+#  Defaults to the name/title of the resource, prepended by '--'.
+#  Useful for listing things like applied classes. 
+#
+# [*order*]
+#  The order in which our fragment should appear in the target file.
+#  Defaults to '10'.
+#
+define motd::register(
+  $content='"    -- ${name}\n"', 
+  $order=10,
+) {
 
+  include ::motd
   concat::fragment{"motd_fragment_${name}":
     target  => $motd::motd,
-    content => $body,
+    content => $content,
   }
+  
 }
